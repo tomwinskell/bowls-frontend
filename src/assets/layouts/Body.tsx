1ee327a86f8459ce0c576f83c74c7c6af53773d5
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-01-14 12:30:13 -0500

    Contact form created, styled and tested. Contact form sends with npm package emailjs.

--- src/assets/layouts/Body.tsx
+++ src/assets/layouts/Body.tsx
@@ -1,11 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import Content from '../components/Content';
+import { device } from '../device';
 
 const divStyle = css({
-  padding: '1rem',
+  padding: '2rem 3rem 4rem 3rem',
   display: 'flex',
   justifyContent: 'center',
+  [device.mobile]: {
+    padding: '1rem 2rem 3rem 2rem',
+  }
 });
 interface ContentProps {
   children: React.ReactNode;
--- src/assets/pages/Contact.tsx
+++ src/assets/pages/Contact.tsx
@@ -1,15 +1,93 @@
 /** @jsxImportSource @emotion/react */
-
+import { css } from '@emotion/react';
 import Body from '../layouts/Body';
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
 
-export default function Home() {
-  return (
-    <>
-      <Body>
-        <h2>Club Contact Information</h2>
-        <p>Tel: 07356 073610</p>
-        <p>Email: mundesleyhaigbowlsclub at gmail.com</p>
-      </Body>
-    </>
-  );
+const formStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  fontSize: '1rem',
+  '& input, textarea': {
+    borderRadius: '5px',
+    border: 'thin solid gray',
+    fontSize: '1rem',
+    padding: '.5rem',
+    marginBottom: '1rem',
+  },
+  '& button': {
+    fontSize: '1rem',
+    borderRadius: '20px',
+    border: 'none',
+    backgroundColor: 'var(--orange)',
+    padding: '.5rem 2rem',
+    width: '100%',
+  },
+});
+
+export default function Contact() {
+  const [sent, setSent] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!formRef.current) return;
+
+    emailjs
+      .sendForm('service_364ohnh', 'template_e00j55m', formRef.current, {
+        publicKey: 'nYwWgOkehNBXEy1MO',
+      })
+      .then(
+        () => {
+          console.log('Success');
+        },
+        (error) => {
+          console.log('Failed', error.text);
+        }
+      );
+
+    setSent(true);
+  };
+
+  if (sent) {
+    return (
+      <>
+        <Body>
+          <p>Thank you for contacting us.</p>
+
+          <p>We will reply to you soon.</p>
+        </Body>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Body>
+          <h2>Club Contact Information</h2>
+          <p>
+            Use the form below to contact us. We look forward to hearing from
+            you!
+          </p>
+
+          <form ref={formRef} css={formStyle} onSubmit={sendEmail}>
+            <label htmlFor="">Name</label>
+            <input type="text" name="name" required />
+            <label htmlFor="">Email</label>
+            <input type="email" name="email" required />
+            <label htmlFor="">Phone Number</label>
+            <input type="tel" name="phone" />
+            <label htmlFor="">Message</label>
+            <textarea
+              rows={5}
+              css={{ resize: 'none' }}
+              name="message"
+              required
+            />
+            <button>Send Message</button>
+          </form>
+        </Body>
+      </>
+    );
+  }
 }
